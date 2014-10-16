bc2f089ca6605e19729956c425a7e71c0044890e
Pavel Tarno <paveltarno@gmail.com>
2014-10-16 16:39:00 +0300

    Major fixes/compatibility for doorstep

--- sample/db/samples/payment_methods.rb
+++ sample/db/samples/payment_methods.rb
@@ -1,43 +1,43 @@
-Spree::Gateway::Bogus.create!(
-  {
-    :name => "כרטיס אשראי",
-    :description => "בדיקת סליקה לכרטיס אשראי.",
-    :environment => "development",
-    :active => true
-  }
-)
+# Spree::Gateway::Bogus.create!(
+#   {
+#     :name => "כרטיס אשראי",
+#     :description => "בדיקת סליקה לכרטיס אשראי.",
+#     :environment => "development",
+#     :active => true
+#   }
+# )
 
-Spree::Gateway::Bogus.create!(
-  {
-    :name => "כרטיס אשראי",
-    :description => "בדיקת סליקה לכרטיס אשראי.",
-    :environment => "production",
-    :active => true
-  }
-)
+# Spree::Gateway::Bogus.create!(
+#   {
+#     :name => "כרטיס אשראי",
+#     :description => "בדיקת סליקה לכרטיס אשראי.",
+#     :environment => "production",
+#     :active => true
+#   }
+# )
 
-Spree::Gateway::Bogus.create!(
-  {
-    :name => "כרטיס אשראי",
-    :description => "בדיקת סליקה לכרטיס אשראי.",
-    :environment => "staging",
-    :active => true
-  }
-)
+# Spree::Gateway::Bogus.create!(
+#   {
+#     :name => "כרטיס אשראי",
+#     :description => "בדיקת סליקה לכרטיס אשראי.",
+#     :environment => "staging",
+#     :active => true
+#   }
+# )
 
-Spree::Gateway::Bogus.create!(
-  {
-    :name => "כרטיס אשראי",
-    :description => "בדיקת סליקה לכרטיס אשראי.",
-    :environment => "test",
-    :active => true
-  }
-)
+# Spree::Gateway::Bogus.create!(
+#   {
+#     :name => "כרטיס אשראי",
+#     :description => "בדיקת סליקה לכרטיס אשראי.",
+#     :environment => "test",
+#     :active => true
+#   }
+# )
 
-Spree::PaymentMethod::Check.create!(
-  {
-    :name => "צ׳ק",
-    :description => "שלם בצ׳ק.",
-    :active => true
-  }
-)
+# Spree::PaymentMethod::Check.create!(
+#   {
+#     :name => "צ׳ק",
+#     :description => "שלם בצ׳ק.",
+#     :active => true
+#   }
+# )
