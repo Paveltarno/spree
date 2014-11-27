881f8f0be53787a78e97d45c3f34575fc73acbe6
Pavel Tarno <paveltarno@gmail.com>
2014-11-27 19:18:37 +0200

    Changed sample data

--- sample/db/samples/stock.rb
+++ sample/db/samples/stock.rb
@@ -1,13 +1,15 @@
 Spree::Sample.load_sample("variants")
 
-location = Spree::StockLocation.first_or_create! name: 'סניף ראשי'
+location = Spree::StockLocation.any? ? Spree::StockLocation.first : Spree::StockLocation.new(name: 'סניף ראשי')
 location.active = true
 location.country = Spree::Country.where(iso: 'IL').first
+location.zone = Spree::Zone.first
 location.save!
 
 location2 = Spree::StockLocation.new(name: 'סניף משני')
 location2.active = true
 location2.country = Spree::Country.where(iso: 'IL').first
+location2.zone = Spree::Zone.last
 location2.save!
 
 Spree::Variant.all.each do |variant|
